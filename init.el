5c1629b765abad2735b320246750e31cedb1e9c7
yl <1016076582@qq.com>
2023-08-15 17:49:58 +0800

    move to org file

--- init.el
+++ init.el
@@ -1,49 +1,40 @@
-(add-to-list 'load-path (expand-file-name "init" user-emacs-directory))
+;;; init.el --- Where all the magic begins
+;;
+;; This file loads Org and then loads the rest of our Emacs initialization from Emacs lisp
+;; embedded in literate Org files.
+
+;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   '(rust-mode flycheck-rust f yasnippet markdown-mode smartparens
-	       htmlize org-yt flycheck-guile flycheck olivetti
-	       geiser-guile projectile super-save pdf-tools dirvish
-	       zenburn-theme dashboard geiser-mit ob-async
-	       macrostep-geiser orderless vertico marginalia geiser
-	       company which-key))
- '(package-vc-selected-packages
-   '((org-yt :vc-backend Git :url
-	     "https://github.com/TobiasZawada/org-yt"))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
-(defmacro eat/pkg (package &optional vc)
-  `(unless (package-installed-p ',package)
-     (unless (memq ',package package-archive-contents)
-       (package-refresh-contents))
-     (if ,vc (package-vc-install ,vc)
-       (package-install ',package))))
+(let* ((org-dir (expand-file-name
+                 "lisp" (expand-file-name
+                         "org" (expand-file-name
+                                "src" dotfiles-dir))))
+       (org-contrib-dir (expand-file-name
+                         "lisp" (expand-file-name
+                                 "contrib" (expand-file-name
+                                            ".." org-dir))))
+       (load-path (append (list org-dir org-contrib-dir)
+                          (or load-path nil))))
+  ;; load up Org and Org-babel
+  (require 'org)
+  (require 'ob-tangle))
 
-;; (eat/pkg org-yt "https://github.com/TobiasZawada/org-yt")
+;; load up all literate org-mode files in this directory
+(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
-;; (unless (package-installed-p 'org-yt)
-;;   (package-vc-install "https://github.com/Tobiaszawada/org-yt"))
-
-(require 'init-emacs)
-(require 'init-scheme)
-(require 'init-docs)
-(require 'init-org)
-(require 'init-lspce)
+;; init.el ends here
--- init/init-docs.el
+++
@@ -1,4 +0,0 @@
-(pdf-tools-install)
-;; (add-hook 'doc-view-mode-hook 'pdf-tools-install)
-
-(provide 'init-docs)
