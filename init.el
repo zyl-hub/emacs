85b4df49a19e4869612c0083d08d757618bc1887
yl <1016076582@qq.com>
2023-09-16 01:05:03 +0800

    Add avy to move faster

--- init.el
+++ init.el
@@ -11,12 +11,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307" "f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
+   '("524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307"
+     "f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7"
+     default))
  '(inhibit-startup-screen t)
+ '(org-agenda-files '("~/org/gtd.org"))
  '(package-selected-packages
-   '(geiser flycheck yasnippet which-key vertico toc-org super-save solarized-theme smartparens rust-mode projectile pdf-tools org-yt orderless olivetti ob-async meow markdown-mode marginalia macrostep-geiser lispy htmlize geiser-mit format-all flycheck-rust f dirvish dashboard company))
+   '(geiser flycheck yasnippet which-key vertico toc-org super-save
+	    solarized-theme smartparens rust-mode projectile pdf-tools
+	    org-yt orderless olivetti ob-async meow markdown-mode
+	    marginalia macrostep-geiser lispy htmlize geiser-mit
+	    format-all flycheck-rust f dirvish dashboard company))
  '(package-vc-selected-packages
-   '((org-yt :vc-backend Git :url "https://github.com/TobiasZawada/org-yt"))))
+   '((org-yt :vc-backend Git :url
+	     "https://github.com/TobiasZawada/org-yt"))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
