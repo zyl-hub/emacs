10ed7681197f1761074e63bbdb75197507f30710
yl <1016076582@qq.com>
2023-09-22 01:00:34 +0800

    add doom theme
    doom theme in org mode works better

--- init.el
+++ init.el
@@ -11,20 +11,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307"
-     "f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7"
-     default))
+   '("7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e" "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "4b6cc3b60871e2f4f9a026a5c86df27905fb1b0e96277ff18a76a39ca53b82e1" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307" "f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
  '(inhibit-startup-screen t)
  '(org-agenda-files '("~/org/gtd.org"))
  '(package-selected-packages
-   '(geiser flycheck yasnippet which-key vertico toc-org super-save
-	    solarized-theme smartparens rust-mode projectile org-yt
-	    orderless olivetti ob-async meow markdown-mode marginalia
-	    macrostep-geiser lispy htmlize geiser-mit format-all
-	    flycheck-rust f dirvish dashboard company))
+   '(doom-themes org-download racket-mode racket geiser flycheck yasnippet which-key vertico toc-org super-save smartparens rust-mode projectile org-yt orderless olivetti ob-async meow markdown-mode marginalia macrostep-geiser lispy htmlize geiser-mit format-all flycheck-rust f dirvish dashboard company))
  '(package-vc-selected-packages
-   '((org-yt :vc-backend Git :url
-	     "https://github.com/TobiasZawada/org-yt"))))
+   '((org-yt :vc-backend Git :url "https://github.com/TobiasZawada/org-yt"))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
